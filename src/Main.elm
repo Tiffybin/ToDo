ccb5978125a6925fe125bf771d0c2c9422379d20
tiffanyuong <tiffuong2@gmail.com>
2024-09-14 18:26:57 -0400

    adding time

--- src/Main.elm
+++ src/Main.elm
@@ -2,22 +2,30 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (attribute, class, disabled, for, form, id, name, placeholder, tabindex, type_, value)
+import Html.Attributes exposing (attribute, class, for, id, name, placeholder, tabindex, type_, value)
 import Html.Events exposing (onClick, onInput)
 import Html5.DragDrop
 import Json.Encode
 import List.Extra
+import Task
+import Time
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = { items = [], userInput = "", dragDrop = Html5.DragDrop.init }, update = update, view = view }
+    Browser.element
+        { init = \_ -> ( { items = [], userInput = "", dragDrop = Html5.DragDrop.init, time = Time.millisToPosix 0 }, Cmd.none )
+        , subscriptions = \_ -> Sub.none
+        , update = update
+        , view = view
+        }
 
 
 type alias Model =
     { items : List Bullet
     , userInput : String
     , dragDrop : Html5.DragDrop.Model DragId DropId
+    , time : Time.Posix
     }
 
 
@@ -39,6 +47,7 @@ type Msg
     | Add
     | Edit Int String
     | DragDropMsg (Html5.DragDrop.Msg DragId DropId)
+    | Time Time.Posix
 
 
 remove : Int -> List Bullet -> List Bullet
@@ -46,7 +55,7 @@ remove i list =
     List.Extra.removeAt i list
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     let
         x =
@@ -54,23 +63,23 @@ update msg model =
     in
     case msg of
         Delete i ->
-            { model | items = remove i model.items }
+            ( { model | items = remove i model.items }, Cmd.none )
 
         Add ->
-            { model | items = { title = model.userInput } :: model.items }
+            ( { model | items = { title = model.userInput } :: model.items }, Cmd.none )
 
         Change userI ->
-            { model | userInput = userI }
+            ( { model | userInput = userI }, Cmd.none )
 
         Edit i newS ->
-            { model | items = List.Extra.updateAt i (editBullet newS) model.items }
+            ( { model | items = List.Extra.updateAt i (editBullet newS) model.items }, Cmd.none )
 
         DragDropMsg dragDrop ->
             let
                 ( model_, result ) =
                     Html5.DragDrop.update dragDrop model.dragDrop
             in
-            { model
+            ( { model
                 | dragDrop = model_
                 , items =
                     case result of
@@ -79,7 +88,17 @@ update msg model =
 
                         Nothing ->
                             model.items
-            }
+              }
+            , Cmd.none
+            )
+
+        Time psx ->
+            ( Debug.log "logging 2" { model | time = psx }, getTime )
+
+
+getTime : Cmd Msg
+getTime =
+    Task.perform Time Time.now
 
 
 editBullet : String -> Bullet -> Bullet
@@ -92,6 +111,7 @@ view model =
     div [ class "background" ]
         [ p [ class "text-center fs-1 fw-bold font-monospace text-title " ] [ text "To-Do List" ]
         , div [] []
+        , h1 [] [ text (String.fromInt (Time.posixToMillis model.time)) ]
         , div [ class "d-flex justify-content-center align-items-center" ]
             [ div [ class "d-flex mb-3" ]
                 [ input [ class "form-control me-3", placeholder "Write something", value model.userInput, onInput Change ] []
@@ -99,7 +119,11 @@ view model =
                 ]
             ]
         , div [ class "d-flex justify-content-center" ]
-            [ ul [ class "list-unstyled flex-column align-items-center" ] (List.Extra.interweave (List.indexedMap viewBullet model.items) (List.indexedMap viewDropZone model.items)) ]
+            [ ul [ class "list-unstyled flex-column align-items-center" ]
+                (List.Extra.interweave (List.Extra.initialize (List.length model.items + 1) viewDropZone)
+                    (List.indexedMap viewBullet model.items)
+                )
+            ]
         ]
 
 
@@ -160,7 +184,7 @@ viewModal i b =
         ]
 
 
-moveId : DragId -> DropId -> List Bullet -> List Bullet
+moveId : DragId -> DropId -> List a -> List a
 moveId dragId dropId items =
     let
         to =
@@ -196,8 +220,8 @@ insertItem index item list =
     before ++ [ item ] ++ after
 
 
-viewDropZone : Int -> Bullet -> Html Msg
-viewDropZone index b =
+viewDropZone : Int -> Html Msg
+viewDropZone index =
     hr (Html5.DragDrop.droppable DragDropMsg index ++ [ class "m-2" ]) []
 
 
